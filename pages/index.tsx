8d7dc1f7444b375674d068497354fe5a5e7c2503
Edwin Scharfe <edwin.scharfe@mindsquare.de>
2022-04-27 19:25:12 +0200

    create contentful client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
-export default function Recipes() {
-  return (
-      <div className="recipe-list">
-        Recipe List
-      </div>
-  )
+import {createClient, EntryCollection} from "contentful";
+
+export async function getStaticProps() {
+
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID ? process.env.CONTENTFUL_SPACE_ID : "",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ? process.env.CONTENTFUL_ACCESS_TOKEN : ""
+    });
+
+    const entries = await client.getEntries({
+        content_type: "recipe"
+    });
+
+    return {
+        props: {
+            recipes: entries.items
+        }
+    }
+}
+
+export default function Recipes({recipes}: { recipes: EntryCollection<any> }) {
+    console.log(recipes);
+    return (
+        <div className="recipe-list">
+            Recipe List
+        </div>
+    )
 };
