374503a29f7cb45596b13ef62182570d7386151c
Edwin Scharfe <edwin.scharfe@mindsquare.de>
2022-04-28 18:58:09 +0200

    styled jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,14 @@ export default function Recipes({recipes}: { recipes: Array<any> }) {
             {recipes.map(recipe => (
                 <RecipeCard key={recipe.sys.id} recipe={recipe}/>
             ))}
+
+            <style jsx>{`
+            .recipe-list {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                grid-gap: 20px 6px;
+            } 
+        `}</style>
         </div>
     )
 };
